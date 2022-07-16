2f19b0b327300587fac135533984f2e39a3563ba
mohamed0tarek <92942683+mohamed0tarek@users.noreply.github.com>
2022-07-16 20:01:37 +0200

    Add files via upload

---
+++ Solutions/Main.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] result = new int[nums.length];
+        int sum = 0;
+        for (int i=0; i<nums.length; i++){
+            result[i]=nums[i] + sum;
+            sum += nums[i];
+        }
+        return result;
+    }
+}
