adeb6438141f8b44e4ee14fc37a1ddb8496424e8
mohamed0tarek <92942683+mohamed0tarek@users.noreply.github.com>
2022-07-17 19:53:21 +0200

    Update and rename Main.java to 412.java

--- Solutions/412.java
+++ Solutions/412.java
@@ -1,6 +1,5 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        // ans list
         List<String> ans = new ArrayList<String>();
 
         for (int num = 1; num <= n; num++) {
@@ -11,21 +10,17 @@ class Solution {
             String numAnsStr = "";
 
             if (divisibleBy3) {
-                // Divides by 3, add Fizz
                 numAnsStr += "Fizz";
             }
 
             if (divisibleBy5) {
-                // Divides by 5, add Buzz
                 numAnsStr += "Buzz";
             }
 
             if (numAnsStr.equals("")) {
-                // Not divisible by 3 or 5, add the number
                 numAnsStr += Integer.toString(num);
             }
 
-            // Append the current answer str to the ans list
             ans.add(numAnsStr);
         }
 
