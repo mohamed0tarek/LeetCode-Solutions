f6e3bd515963f10fb5644cb22b305f97335e4da3
mohamed0tarek <92942683+mohamed0tarek@users.noreply.github.com>
2022-07-29 09:34:49 +0200

    Create .java

---
+++ Solutions/.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> hash = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hash.containsKey(nums[i]) && i - hash.get(nums[i]) <= k) {
+                return true;
+            }
+            hash.put(nums[i], i);
+        }
+        return false;
+    }
+}
