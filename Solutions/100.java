07a08b64cdaada8559a28d686a748f70f08e1028
mohamed0tarek <92942683+mohamed0tarek@users.noreply.github.com>
2022-09-10 15:19:00 +0200

    Create 100.java

---
+++ Solutions/100.java
@@ -0,0 +1,5 @@
+public boolean isSameTree(TreeNode p, TreeNode q) {
+	if (p == null || q == null) return p == q;
+	if (p.val != q.val) return false;
+	return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+}
