a3be065eabfe28e86b6dfb17c7ee2ee2469c1df2
mohamed0tarek <92942683+mohamed0tarek@users.noreply.github.com>
2022-09-20 13:00:45 +0200

    Update 1971.java

--- Solutions/1971.java
+++ Solutions/1971.java
@@ -38,3 +38,30 @@ class Solution {
         return res;
     }
 }
+---------------------------------------------------------------------------
+    
+    class Solution {
+    public boolean validPath(int n, int[][] edges, int start, int end) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        for (int i = 0; i < edges.length; i++) {
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        HashSet<Integer> vis = new HashSet<>();
+        q.add(start);
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            vis.add(u);
+            if (u == end) return true;
+            for (int v : adj.get(u)) {
+                if (!vis.contains(v)) {
+                    q.add(v);
+                }
+            }
+        }
+        return false;
+    }
+}
