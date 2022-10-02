cc057fac4b3acdf9361d8a60d69103c40f64d5be
mohamed0tarek <92942683+mohamed0tarek@users.noreply.github.com>
2022-10-02 10:24:43 +0200

    Create 938.java

---
+++ Solutions/938.java
@@ -0,0 +1,11 @@
+class Solution {
+    int sum = 0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) return 0;
+        if (root.val >= low && root.val <= high)
+            sum += root.val;
+        rangeSumBST(root.right, low, high);
+        rangeSumBST(root.left, low, high);
+        return sum;
+    }
+}
