753e524374530d535206f59b2727db4ea5f91fb2
mohamed0tarek <mohamed.tarek72417293@gmail.com>
2024-01-23 15:01:30 +0200

    Update 104.java

--- Solutions/104.java
+++ Solutions/104.java
@@ -1,19 +1,11 @@
 class Solution {
     public int maxDepth(TreeNode root) {
-        int count =1;
-        if (root == null)
-            return 0;
-        return traverse(root, root.left,root.right,count);
+        return maxDepth(root, 0);
     }
-    
-    public int traverse(TreeNode rootN,TreeNode leftN,TreeNode rightN,int count){
-        if(leftN == null && rightN == null)
-            return count;
 
-        count++;
-        int le=0,ri=0;
-        if(leftN != null) {le = traverse(leftN, leftN.left, leftN.right,count);}
-        if(rightN != null) {ri = traverse(rightN, rightN.left, rightN.right,count);}
-        return Math.max(le,ri);
+    private int maxDepth(TreeNode root, int n) {
+        if(root == null) return n;
+        n++;
+        return Math.max(maxDepth(root.left, n), maxDepth(root.right, n));
     }
 }
