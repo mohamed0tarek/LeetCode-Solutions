503a820aa4f090897cd1182e1fd63a33c93f874c
mohamed0tarek <mohamed.tarek72417293@gmail.com>
2024-01-23 15:48:11 +0200

    Create 779.java

---
+++ Solutions/779.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int recursion(int n, int k) {
+        if (n == 1) {
+            return 0;
+        }
+
+        int totalElements = (int) Math.pow(2, (n - 1));
+        int halfElements = totalElements / 2;
+
+        // If the target is present in the right half, we switch to the left half symbol.
+        if (k > halfElements) {
+            return 1 - recursion(n, k - halfElements);
+        }
+
+        // Otherwise, we switch to the previous row.
+        return recursion(n - 1, k);
+    }
+
+    public int kthGrammar(int n, int k) {
+        return recursion(n, k);
+    }
+}
